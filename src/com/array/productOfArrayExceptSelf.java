d888eefcd3c8a978940c1cf30da31fb43ba3334e
Jitesh <jitesh.srivastava@gmail.com>
2022-01-18 14:07:12 +0530

    product of array except self O(n) O(1)

--- src/com/array/productOfArrayExceptSelf.java
+++ src/com/array/productOfArrayExceptSelf.java
@@ -1,22 +1,27 @@
 package com.array;
 
 public class productOfArrayExceptSelf {
+    // Prefix sum
     public static void main(String[] args) {
-
+        int[] array = {1,2,3,4};
+        System.out.println(product(array));
     }
 
-    public static void product(int[] arr) {
-        int[] output = new int[arr.length];
-        int product = 1;
-        for (int i = 1; i < arr.length; i++) {
-            product *= output[i - 1];
-            output[i] = product;
+    public static int[] product(int[] arr) {
+        int n = arr.length;
+        int[] output = new int[n];
+        output[0] = 1;
+        // Find the left product
+        for (int i = 1; i < n; i++) {
+            output[i] = arr[i - 1] * output[i - 1];
         }
 
-        product = 1;
-        for(int i = arr.length - 2; i < arr.length; i--) {
-            product *= output[i + 1];
-            output[i] = product
+        int right = 1;
+
+        for(int i = n - 1; i >= 0 ; i--) {
+            output[i] = output[i] * right;
+            right = right * arr[i];
         }
+        return  output;
     }
 }
